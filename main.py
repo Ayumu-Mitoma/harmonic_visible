ae35e97079d14d120e5f8718dfed99366dc626ad
Ayumu-Mitoma <v24e3028@oita-u.ac.jp>
2025-03-05 17:23:12 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ import numpy as np
 import streamlit as st
 from audio_recorder_streamlit import audio_recorder
 from io import BytesIO
-"""
+import time
 
+
+MAX_TIME = 20
+"""
 cqt = ap.create_CQT(noise_wav_io, C.LOW_TUNING)
 row = ap.search_max_index(cqt)
 #row_84 = ap.create_12_data(row)
@@ -22,16 +25,34 @@ st.header("手順通りに進めてみよう")
 option = st.radio("下から次の操作を選んでね",
                   ["今から音を録音する", "録音した音を選ぶ"])
 
+
 if option == "今から音を録音する":
+    #音を録音する際の処理
     st.subheader("1. 音を録音しよう")
     st.text("ピアノの近くにスマホを置いて録音してみよう")
-    audio_byte = audio_recorder()
-    if audio_byte:
-        with BytesIO(audio_byte)as f:
-            data = ap.byte_to_audio(f)
-        noise_wav_io = ap.noise_reducer(data, num = 0.8)
-        cqt = ap.create_CQT(noise_wav_io, C.LOW_TUNING)
-        st.text(cqt.shape)
+   
+    #ボタンを横並びにする
+    col1, col2 = st.columns(2)
+    start_recording = col1.button("録音開始")
+    end_recording = col2.button("録音停止")
+
+    if start_recording:
+        start_time = time.time()
+        st.session_state["recording"] = True
+    if end_recording:
+        st.session_state["recording"] = False
+    if "recording" in st.session_state and st.session_state["recording"]:
+
+        audio_bytes = audio_recorder()
+        if time.time() - start_time >= MAX_TIME:
+            st.session_state["recording"] = False
+            st.warning("20秒以上は録音できません")
+        if audio_bytes:
+            with BytesIO(audio_bytes)as f:
+                data = ap.byte_to_audio(f)
+            noise_wav_io = ap.noise_reducer(data, num = 0.8)
+            cqt = ap.create_CQT(noise_wav_io, C.LOW_TUNING)
+            st.text(cqt.shape)
 
 if option == "録音した音を選ぶ":
     st.subheader("1. 録音した音声を渡してね")
