874cdff468bcf9e3dd6a7cd6bed01784cae90bb9
Ayumu-Mitoma <v24e3028@oita-u.ac.jp>
2025-03-05 17:28:36 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ from audio_recorder_streamlit import audio_recorder
 from io import BytesIO
 import time
 
-
-MAX_TIME = 20
 """
 cqt = ap.create_CQT(noise_wav_io, C.LOW_TUNING)
 row = ap.search_max_index(cqt)
@@ -31,28 +29,17 @@ if option == "今から音を録音する":
     st.subheader("1. 音を録音しよう")
     st.text("ピアノの近くにスマホを置いて録音してみよう")
    
-    #ボタンを横並びにする
-    col1, col2 = st.columns(2)
-    start_recording = col1.button("録音開始")
-    end_recording = col2.button("録音停止")
-
-    if start_recording:
-        start_time = time.time()
-        st.session_state["recording"] = True
-    if end_recording:
-        st.session_state["recording"] = False
-    if "recording" in st.session_state and st.session_state["recording"]:
-
-        audio_bytes = audio_recorder()
-        if time.time() - start_time >= MAX_TIME:
-            st.session_state["recording"] = False
-            st.warning("20秒以上は録音できません")
-        if audio_bytes:
-            with BytesIO(audio_bytes)as f:
-                data = ap.byte_to_audio(f)
-            noise_wav_io = ap.noise_reducer(data, num = 0.8)
-            cqt = ap.create_CQT(noise_wav_io, C.LOW_TUNING)
-            st.text(cqt.shape)
+    audio_bytes = audio_recorder(
+        energy_threshold=(100.0, -1.0),
+        text="ボタンを押して録音"
+        icon_size="2x"
+    )
+    if audio_bytes:
+        with BytesIO(audio_bytes)as f:
+            data = ap.byte_to_audio(f)
+        noise_wav_io = ap.noise_reducer(data, num = 0.8)
+        cqt = ap.create_CQT(noise_wav_io, C.LOW_TUNING)
+        st.text(cqt.shape)
 
 if option == "録音した音を選ぶ":
     st.subheader("1. 録音した音声を渡してね")
