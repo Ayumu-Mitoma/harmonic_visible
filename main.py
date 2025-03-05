ef9f8a30703c4d5c37f1babebb0aa65a495dafb1
Ayumu-Mitoma <v24e3028@oita-u.ac.jp>
2025-03-05 16:42:02 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ import const as C
 import audio_processing as ap
 import numpy as np
 import streamlit as st
+from audio_recorder_streamlit import st_audio_recorder
 
 """
+wav_io = ap.m4a_to_wav(file)
+noise_wav_io = ap.noise_reducer(wav_io, num = 0.8)
+cqt = ap.create_CQT(noise_wav_io, C.LOW_TUNING)
 row = ap.search_max_index(cqt)
 #row_84 = ap.create_12_data(row)
 row_84 = ap.create_12_data_beta(row)
@@ -22,11 +26,8 @@ st.text("ピアノの近くにスマホを置いて録音してみよう")
 st.subheader("2. 録音した音声を渡してね")
 st.text("ボタンを押して録音した音声を選んでね")
 
-file = st.file_uploader("ボタンを押して録音した音声を選んでね")
+audio = st_audio_recorder()
 
-if file is not None:
-    file_name = file.name.lower()
-    if file_name.endswith(".m4a"):
-        wav_io = ap.m4a_to_wav(file)
-        noise_wav_io = ap.noise_reducer(wav_io, num = 0.8)
-        cqt = ap.create_CQT(noise_wav_io, C.LOW_TUNING)
+if audio:
+    st.audio(audio, format="audio/wav")
+    st.write("録音された音声を再生できます")
